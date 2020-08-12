1fd1935a46a262da43d6ae226bc65d3c269c515c
Cindy Kim <66973895+orangeteddy@users.noreply.github.com>
2020-08-12 01:27:41 -0700

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+//my very first code in C language!
+
+#include <studio.h>
+int main()
+{
+  printf("Hello world!");
+  return 0;
+}
+
+/*It's very tempting to start a comment with #, so I should be careful.
+I am both excited and terrified to look at this kind of raw, low-level language.
+After looking at friendly codes of Python, C really does give me a bit of anxiety.*/
